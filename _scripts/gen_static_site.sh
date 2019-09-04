13dbb0cfd3748fc724e1f3ae1b56e7cb267f71b2
Heiko Becker <hbecker@mpi-sws.org>
2019-09-04 14:20:07 +0200

    Make CakeML blog post public, add new script

--- _scripts/gen_static_site.sh
+++ _scripts/gen_static_site.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-echo "  <div class="divider">
-  </div>
-  <div class="row">
-    <a href="https://data-protection.mpi-klsb.mpg.de/sws/people/hbecker">Data Protection</a>
-    <a href="https://imprint.mpi-klsb.mpg.de/sws/people/hbecker">Imprint</a>
-  </div>" >> ./_includes/main.html
+bundle exec jekyll build -b \/\~hbecker
 
-bundle exec jekyll build
-
-for num in 1 2 3 4 5 6
-do
-    sed '$d' -i _includes/main.html
-done
-#head -n -6 ./_includes/main.html > ./_includes/main.html
+scp -r _site/* hbecker@contact.mpi-sws.org:~/public_html/
